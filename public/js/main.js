450fdafde4a3799152b19ea2d162fd3a18545054
Maxence <lemairemaxence@hotmail.com>
2020-12-30 12:04:00 +0100

    exo ok

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+let nombre = 8.56
+
+// 2
+
+// nombre = Math.round(8.56)
+// console.log(nombre);
+
+// 3
+
+// nombre = Math.round(1 * nombre)
+// console.log(nombre);
+
+// 4 
+
+// nombre = Math.round(0.9 * nombre)
+// console.log(nombre);
+
+// 5
+
+// nombre = parseInt(8.56)
+// console.log(nombre);
+
+// console.log(Math.floor(nombre));
+
+// 6
+
+console.log(Math.random());
+
+// 7
+
+console.log(Math.random() * 100);
+
+// 8
+
+console.log(Math.floor(Math.random() * 100));
+
+// 9
+
+console.log(Math.pow(8, 2));
+
+// 10
+
+console.log(Math.sqrt(9));
+
+// 11
+
+console.log(Math.abs(-1));
+
+// 12
+
+console.log(Math.max(-2, 1000, 8, 57));
+
+// 13
+
+console.log(Math.min(-2, 1000, 8, 57));
